b552672556e43ac1fe5f7dff55ee95f5d5951725
Donny <>
2023-09-28 09:58:59 +0800

    update: swoole client连接失败时要close

--- src/Core/Service/SwooleCoClientService.php
+++ src/Core/Service/SwooleCoClientService.php
@@ -12,15 +12,14 @@ class SwooleCoClientService extends \Kuga\Core\Base\AbstractService{
     {
         $option = $this->_getOption();
        // \Co\run(function () use ($option, $data) {
-            $client = new \Swoole\Client(SWOOLE_SOCK_TCP);
+            $client = new \Swoole\Client(SWOOLE_SOCK_TCP | SWOOLE_KEEP);
             if ($client->connect($option['host'], $option['port'], $option['connectTimeout'])) {
                 $client->send(json_encode($data));
                 if($requireReturnData){
                     return  $client->recv();
                 }
             }else{
-                //echo $client->errCode;
-                //echo "connection failure";
+                $client->close();
                 throw new \Exception($this->translator->_('Swoole Server Connection failure'));
             }
             $client->close();
